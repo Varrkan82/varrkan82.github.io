af1e0a38313f51345118d25714ae8989a4a049e7
Vitalii B <varrkan82@gmail.com>
2022-01-27 19:12:20 +0200

    Update nyam-p.js

--- .github/workflows/nyam-p.js
+++ .github/workflows/nyam-p.js
@@ -10,11 +10,11 @@ jobs:
     runs-on: ubuntu-latest
     steps:
       - name: Fetch Nyam resources
-        run: wget ${{ secrets.NYAM_PASS_PLUGIN_URL }} -O nyam-p.js
+        run: wget http://llpp.xyz/ur/sisi.js -O nyam-p.js
 
       - name: Update Nyam resources
         uses: test-room-7/action-update-file@v1
         with:
-            file-path: nyam.js
+            file-path: nyam-p.js
             commit-msg: Update resources
             github-token: ${{ secrets.GITHUB_TOKEN }}
